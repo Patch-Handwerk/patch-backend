cf7761d92b9899bedbc9671547bc8dbc218dfb91
Muhammad Asaad <asaadjawed70@gmail.com>
2025-09-01 18:41:20 +0200

    remove imports not used

--- src/modules/evaluation/dto/index.ts
+++ src/modules/evaluation/dto/index.ts
@@ -1,5 +1,2 @@
 export * from './calculate-progress.dto';
-export * from './get-phases.dto';
-export * from './get-subphases.dto';
-export * from './get-question.dto';
 export * from './evaluation-response.dto';
